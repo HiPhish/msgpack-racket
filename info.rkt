588def4848118799c06dbbc7e6d476d5a5438f68
HiPhish <hiphish@Aleksandars-iMac.local>
2017-07-02 18:32:48 +0200

    Refine directory structure

--- info.rkt
+++ info.rkt
@@ -1,5 +1,5 @@
 #lang info
 
-(define collection "msgpack")
-(define pkt-desc "Implementation of MessagePack for Racket")
+(define collection 'multi)
+(define pkg-desc "Implementation of MessagePack for Racket")
 (define version "0.0")
